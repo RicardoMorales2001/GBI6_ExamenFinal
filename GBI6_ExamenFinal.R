fa7d60de748f52adb4385267cb7b7011fae5a339
RicardoMorales2001 <angel.morales@est.ikiam.edu.ec>
2022-03-16 23:51:43 -0600

    Se Realizo los lietrales 1.1 y 1.2

--- GBI6_ExamenFinal.R
+++ GBI6_ExamenFinal.R
@@ -1,1 +1,11 @@
+install.packages("readxl")
+library(readxl)             
+# Defina su directorio de trabajo
+getwd()
+# Cargar la data "mRNA_export.csv"
+file.choose()
+dt <- "C:\\Users\\RICARDO\\Documents\\GitHub\\GBI6_ExamenFinal\\data\\mRNA_expr.csv"
+rd <- read.csv(dt)
+
+df <-read.csv(file = 'data/mRNA_expr.csv')
 # Ejercicio 1
