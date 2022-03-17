04e67892df0b3817a031835030de06f541826ab6
RicardoMorales2001 <angel.morales@est.ikiam.edu.ec>
2022-03-17 11:26:13 -0600

    Resolución pregunta 1.3

--- GBI6_ExamenFinal.R
+++ GBI6_ExamenFinal.R
@@ -1,6 +1,9 @@
 install.packages("readxl")
+install.packages("tidyr")
 library(readxl)             
 library(reshape2)
+library(tidyr)
+library(dplyr)
 # Pregunta 1
 # 1.1 Defina su directorio de trabajo
 getwd()
@@ -8,8 +11,9 @@ getwd()
 # 1.2 Cargar la data "mRNA_export.csv"
 file.choose()
 dt <- "C:\\Users\\RICARDO\\Documents\\GitHub\\GBI6_ExamenFinal\\data\\mRNA_expr.csv"
-rd <- read.csv(dt)
+df <- read.csv(dt)
 
 # 1.3 Cree una función de nombre long_df para transformar la data df de la forma wide a long.
-longrd <- melt(rd, id.vars= "bcr_patient_barcode")  
-view(longrd)
+long_data <- gather(df,  GEN, expresion_level,GATA3, PTEN, XBP1, ESR1, MUC1, FN1, GAPDH, factor_key=TRUE)
+long_data1 <- select(long_data, -bcr_patient_barcode)
+View(long_data1)
