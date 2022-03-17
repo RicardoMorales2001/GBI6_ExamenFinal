9f19fa06f011b86ade511d4cd91041d2d47c6a40
RicardoMorales2001 <angel.morales@est.ikiam.edu.ec>
2022-03-17 13:58:47 -0600

    Se realizo la pregunta 2.1

--- GBI6_ExamenFinal.R
+++ GBI6_ExamenFinal.R
@@ -3,6 +3,7 @@ install.packages("tidyr")
 install.packages("readr")
 install.packages("sjPlot")
 install.packages("sjmisc")
+install.packages("ggplot2")
 library(readxl)             
 library(reshape2)
 library(tidyr)
@@ -10,16 +11,17 @@ library(dplyr)
 library(readr)
 library(sjPlot)
 library(sjmisc)
-# Pregunta 1
-# 1.1 Defina su directorio de trabajo
+library(ggplot2)
+#### Pregunta 1
+### 1.1 Defina su directorio de trabajo
 getwd()
 
-# 1.2 Cargar la data "mRNA_export.csv"
+### 1.2 Cargar la data "mRNA_export.csv"
 file.choose()
 dt <- "C:\\Users\\RICARDO\\Documents\\GitHub\\GBI6_ExamenFinal\\data\\mRNA_expr.csv"
 df <- read.csv(dt)
 
-# 1.3 Cree una función de nombre long_df para transformar la data df de la forma wide a long.
+### 1.3 Cree una función de nombre long_df para transformar la data df de la forma wide a long.
 long_df <-function(df) {
   library(tidyr)
   library(dplyr)
@@ -30,7 +32,32 @@ long_df <-function(df) {
 }
 View(long_data1)
 
-# 1.4 Genere la data df_long utilizandola función long_df
+### 1.4 Genere la data df_long utilizandola función long_df
 df_long <- long_df(df)
 test <- df_long %>% group_by(gen)
 
+### 1.5 A partir de la data df_long, genere un Descriptive table (librería sjPlot)
+### y guárdelo en el subdirectorio "result" con el nombre mRNA_expr_summary.doc 
+### (previamente debe seleccionar las celdas adecuadas con funciones de la librería
+### dplyr en conjunto con el operador %>%).
+c_m <- test %>% group_by(gen) %>% mutate(row = row_number()) %>%
+  tidyr::pivot_wider(names_from = gen, values_from = expresion_level) %>%
+  select(-row)
+
+descr(c_m) %>% tab_df(title = "Estadistica del cancer de MAMA y el nivel de expresion de los diferentes genes",
+                              file = "mRNA_expr_summary.doc") 
+
+### 1.6 Interprete los resultados de la tabla resultante de la sección 1.5.
+
+
+#### Pregunta 2 VISUALIZACION DE DATOS
+### 2.1 Cree la funcion tcga_boxplots para visualizar boxplots y jitterplots. 
+### El único parámetro de la función es dataset. La función debe crear un boxplot 
+### de los niveles de expresión para cada gen.
+tcga_boxplots <- function(dataset){
+  library(ggplot2)
+  return(ggplot (dataset, aes(x = gen, y = expresion_level, col = gen)) + labs(x="Gen", y = "Expression level")
+         + geom_boxplot() + geom_jitter(size=0.20))
+}
+tcga_boxplots(df_long)
+View(tcga_boxplots)
