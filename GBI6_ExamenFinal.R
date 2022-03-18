ebf84b39824aaca7988bf448c7cdd0056db18f6c
RicardoMorales2001 <angel.morales@est.ikiam.edu.ec>
2022-03-17 18:22:20 -0600

    Se realizo la pregunta 2.1

--- GBI6_ExamenFinal.R
+++ GBI6_ExamenFinal.R
@@ -30,7 +30,6 @@ long_df <-function(df) {
   long_data1 <- select(long_data, -1)
   return (long_data1)
 }
-View(long_data1)
 
 ### 1.4 Genere la data df_long utilizandola función long_df
 df_long <- long_df(df)
@@ -44,7 +43,7 @@ c_m <- test %>% group_by(gen) %>% mutate(row = row_number()) %>%
   tidyr::pivot_wider(names_from = gen, values_from = expresion_level) %>%
   select(-row)
 
-descr(c_m) %>% tab_df(title = "Estadistica del cancer de MAMA y el nivel de expresion de los diferentes genes",
+descr(c_m) %>% tab_df(title = "Estadistica descriptiva del cancer de MAMA y su nivel de expresion de los diferentes genes",
                               file = "mRNA_expr_summary.doc") 
 
 ### 1.6 Interprete los resultados de la tabla resultante de la sección 1.5.
@@ -55,9 +54,27 @@ descr(c_m) %>% tab_df(title = "Estadistica del cancer de MAMA y el nivel de expr
 ### El único parámetro de la función es dataset. La función debe crear un boxplot 
 ### de los niveles de expresión para cada gen.
 tcga_boxplots <- function(dataset){
-  library(ggplot2)
-  return(ggplot (dataset, aes(x = gen, y = expresion_level, col = gen)) + labs(x="Gen", y = "Expression level")
+  return(ggplot (dataset, aes(x = gen, y = expresion_level, color = gen)) + labs(x="Gen", y = "Expression level")
          + geom_boxplot() + geom_jitter(size=0.20))
 }
 tcga_boxplots(df_long)
-View(tcga_boxplots)
+
+### 2.2 Utilizando la función lapply() genera la lista cancertype_boxplots con 
+### las gráficas para cada tipo de cancer (cada tipo de dataset).
+tipo_de_cancer <- list(
+  BRCA <- filter(df_long, dataset == "BRCA"),
+  OV <- filter(df_long, dataset == "OV"),
+  LUSC <- filter(df_long, dataset == "LUSC"),
+  KIPAN <- filter(df_long, dataset == "KIPAN"),
+  KIRP <- filter(df_long, dataset == "KIRP"),
+  UCEC <- filter(df_long, dataset == "UCEC")
+)
+
+cancertype_boxplots = lapply(tipo_de_cancer, function(x) {ggplot(x, aes(dataset, expresion_level, col = dataset)) +
+    geom_boxplot() + theme(legend.position='none') + labs(x = "", y = "Expression level")})
+
+### 2.3 Guarde en el subdirectorio result el tercer plot de la lista 
+### cancertype_boxplots con el nombre boxplot3.png. La definición de a figura 
+### debe ser de 300dpi.
+cancertype_boxplots[3]
+cancertype_boxplots[2]
